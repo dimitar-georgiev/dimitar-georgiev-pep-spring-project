7ddf55865ddc0dfddc9e88182d4f94749221b4b1
Dimitar Georgiev <dimitar.georgiev.du@gmail.com>
2025-03-12 00:40:36 +0000

    added pseudo routes

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -8,5 +8,27 @@ package com.example.controller;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    // User Registration
+    // POST localhost:8080/register
 
+    // Login
+    // POST localhost:8080/login
+
+    // Create New Message
+    // POST localhost:8080/messages
+
+    // Get All Messages
+    // GET localhost:8080/messages
+
+    // Get Message By ID
+    // GET localhost:8080/messages/{message_id}
+
+    // Delete Message By ID
+    // DELETE localhost:8080/messages/{message_id}
+
+    // Update Message By ID
+    // PATCH localhost:8080/messages/{message_id}
+
+    // Get All Messages By User ID
+    // GET localhost:8080/accounts/{account_id}/messages
 }
