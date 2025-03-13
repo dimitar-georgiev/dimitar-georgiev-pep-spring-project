d1054847e38abac2f52d319b55ca1828c5f9998a
Dimitar Georgiev <dimitar.georgiev.du@gmail.com>
2025-03-13 09:47:13 +0000

    extended JpaRepository and injected repositories into services

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,7 +1,16 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.repository.MessageRepository;
 
 @Service
 public class MessageService {
+    private final MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 }
