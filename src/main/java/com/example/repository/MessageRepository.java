bc8ee5208d690a93f807f5bb66a14c1b11fa0bd9
Dimitar Georgiev <dimitar.georgiev.du@gmail.com>
2025-03-15 00:15:59 +0000

    submit_adjustment

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.entity.Message;
 
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
     List<Message> findByPostedBy(Integer postedBy);
